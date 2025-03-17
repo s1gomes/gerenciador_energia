24d4b6bb4d2787b4ccf3d297f91d80b890ed89f5
s1gomes <sara.gomes@uerr.edu.br>
2025-03-17 14:23:52 -0400

    list build

--- lib/core/constants/constants.dart
+++
@@ -1,4 +0,0 @@
-const String newsAPIBaseURL = 'https://newsapi.org/v2';
-const String newsAPIKey = '5d23f21f43cf4b778312b3a91f25f6c8';
-const String countryQuery = 'us';
-const String categoryQuery= 'general';
